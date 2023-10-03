ff12a8da280722df596cabfbe879ad8922a8fcf7
@ziajoriii7-ggg <cybershadowprogress@gmail.com>
2023-10-03 13:28:01 -0500

    ADD Set-GitUser.ps1

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # powershell-scripts
+
+true:
+toggle-oh-my-posh.ps1 -activate $true
+
+false: toggle-oh-my-posh.ps1 -activate $false
+
+
+folder on PATH:
+$newPath = "D:\@ziajoriii7-ggg\powershell-scripts"; $envPath = [System.Environment]::GetEnvironmentVariable('Path', 'User'); if (-not $envPath.Contains($newPath)) { [System.Environment]::SetEnvironmentVariable('Path', "$envPath;$newPath", 'User') }
---
+++ Set-GitUser.ps1
@@ -0,0 +1,27 @@
+$currentPath = Get-Location
+$userConfig = @{
+    "@aleeeec02" = @{
+        Name = "aleeeec02"
+        Email = "aleeeec02@example.com"
+        Remote = "git@github.com-aleeeec02:"
+    }
+    "@hiyorijl" = @{
+        Name = "hiyorijl"
+        Email = "hiyorijl@example.com"
+        Remote = "git@github.com-hiyorijl:"
+    }
+    "@ziajoriii7-ggg" = @{
+        Name = "ziajoriii7-ggg"
+        Email = "ziajoriii7-ggg@example.com"
+        Remote = "git@github.com-ziajoriii7-ggg:"
+    }
+}
+
+foreach ($key in $userConfig.Keys) {
+    if ($currentPath -match $key) {
+        git config user.name $userConfig[$key]["Name"]
+        git config user.email $userConfig[$key]["Email"]
+        git remote set-url origin $($userConfig[$key]["Remote"] + (Get-Item $currentPath).Name + ".git")
+        break
+    }
+}
