0b1827981645e265fb00f6c9d9ebc47559ee93e3
ziajoriii7 <cybershadowprogress@gmail.com>
2024-02-09 23:37:24 -0500

    UPDATE loltre.ps1; Add feature: be able to add paths as arguments -p or -path

--- loltre.ps1
+++ loltre.ps1
@@ -2,20 +2,48 @@ param (
   [Alias("d")][switch]$dirs,
   [Alias("f")][switch]$files,
   [Alias("s")][int]$start = 1,
-  [Alias("e")][int][Parameter(Position=0)]$end
+  [Alias("e")][int][Parameter(Position=0)]$end,
+  [Alias("p")][string]$path = (Get-Location)
 )
 
-function Get-TotalItemCount {
-  $totalDirs = (Get-ChildItem -Directory).Count
-  $totalFiles = (Get-ChildItem -File).Count
-  $total = (Get-ChildItem).Count 
-  $currentDirName = Split-Path -Path (Get-Location) -Leaf
+function Center-Text {
+  param (
+    [string]$text
+  )
 
+  $windowWidth = $Host.UI.RawUI.WindowSize.Width 
+  $textLength = $text.Length 
+  $leftPadding = ($windowWidth - $textLength) / 2 - 6
+  $paddedText = " " * [Math]::Floor($leftPadding) + $text
+  Write-Host "$paddedText" -NoNewline # ForegroundColor DarkMagenta
+
+  }
+
+function Get-TotalItemCount {
+  # Center-Text "───────── ⋆⋅λ⋅⋆ ────────"
+  $totalDirs = (Get-ChildItem -Directory -Path $path).Count
+  $totalFiles = (Get-ChildItem -File -Path $path).Count
+  $total = (Get-ChildItem -Path $path).Count 
+  $currentDirName = Split-Path -Path $path -Leaf
   # Write-Host "`n"
-    Write-Host "`n$currentDirName's" -NoNewline # -ForegroundColor DarkMagenta
+    # Center-Text "$currentDirName's: "
+    Write-Host " "
+    Write-Host "$currentDirName" -NoNewline -ForegroundColor DarkMagenta
+    Write-Host "'s: "
+    $separatorDir = "─" * ($currentDirName.Length + 3)
+    Write-Host $separatorDir
+    Write-Host " • " -NoNewline
+    Write-Host "[ $total ]" -ForegroundColor DarkMagenta -NoNewline
+    Write-Host " items / [ $end ] lim"
+  # Write-Host "`n"
+    # Write-Host " ↳ items: " -NoNewline
     # Write-Host " has" -NoNewline
-    Write-Host " $total " -NoNewline # -ForegroundColor DarkMagenta
-    Write-Host "items `n ↳ $totalFiles files / $totalDirs dirs`n"
+    # Write-Host " [ $total ] " -NoNewline -ForegroundColor DarkMagenta
+    Write-Host " • " -NoNewline
+    Write-Host "[ $totalFiles ]" -ForegroundColor Cyan -NoNewline
+    Write-Host " files / " -NoNewline -ForegroundColor Gray
+    Write-Host "[ $totalDirs ]" -NoNewline -ForegroundColor Magenta 
+    Write-Host " dirs `n" -ForegroundColor Gray
 }
 
 
@@ -26,25 +54,25 @@ function loltre {
     $dirs = $true
     $files = $true
 
-Get-TotalItemCount
-    Write-Host "Processing: " -NoNewline
-    Write-Host "Directories:" -ForegroundColor Magenta -NoNewline
-    Write-Host ", Files" -ForegroundColor Cyan
+    Get-TotalItemCount -Path $path
+    # Write-Host "Processing: " -NoNewline
+    # Write-Host "Directories," -ForegroundColor Magenta -NoNewline
+    # Write-Host " Files" -ForegroundColor Cyan
   }
   elseif ($dirs) {
     $files = $false
-Get-TotalItemCount
+    Get-TotalItemCount -Path $path
     Write-Host "Processing: " -NoNewline
     Write-Host "Directories" -ForegroundColor Magenta
   }
   elseif ($files) {
-Get-TotalItemCount
+    Get-TotalItemCount -Path $path
     $dirs = $false
     Write-Host "Processing: " -NoNewline
     Write-Host "Files" -ForegroundColor Cyan
   }
 
-  Get-ChildItem | Sort-Object LastWriteTime -Descending | ForEach-Object {
+  Get-ChildItem -Path $path | Sort-Object LastWriteTime -Descending | ForEach-Object {
     $count++
     
     if ($end -and $count -gt $end) { return }
@@ -63,7 +91,9 @@ Get-TotalItemCount
     $formattedCount = "{0,2}|" -f $count
 
     }
+
   }
+  # Center-Text "───────── ⋆⋅λ⋅⋆ ────────"
 }
 
 loltre
