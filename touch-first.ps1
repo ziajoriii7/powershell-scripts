6100f725681dfc335121e99d339a0b78c2ef74a8
ziajoriii7 <cybershadowprogress@gmail.com>
2024-02-11 17:17:48 -0500

    FIX-BUG MAINLY + renamed + small changes (2): (1.) BUG touch-first.ps1; process adequately the README.md headers, (2.) Rename multiple-yt-download.ps1 to web-down.ps1

--- touch-first.ps1
+++ touch-first.ps1
@@ -7,21 +7,30 @@ foreach ($FileName in $FileNames) {
   Write-Host "Processing file: $FileName"
   $isNewFile = $false
 
-  if (-not (Test-Path $FileName)) {
-      Write-Host "Creating new file: $FileName"
-      New-Item -ItemType file -Path $FileName
-      $isNewFile = $true
-  } else {
+  $absoluteFileName = $null
+
+  if (Test-Path $FileName) {
+      $absolutePath = Resolve-Path $FileName
     Write-Host "File exists: $FileName"
+  } else {
+        Write-Host "Creating new file: $FileName"
+        $newFile = New-Item -ItemType file -Path $FileName
+        $absolutePath = $newFile.FullName
+        $isNewFile = $true
   }
   
   $fileBaseName = [System.IO.Path]::GetFileNameWithoutExtension($FileName)
-  $header = "# $fileBaseName`n"
-  if ((Split-Path $FileName -Leaf) -eq "README.md") {
-    $parentDirName = Split-Path $FileName -Parent | Split-Path -Leaf
-    $header = "# $parentDirName`n"
-    Write-Host "Header for README.md $header"
+
+    $header = "# $fileBaseName`n"
+
+    $parentPath = Split-Path $absolutePath -Parent
+    $parentDirName = Split-Path $parentPath -Leaf
+
+  if ((Split-Path $absolutePath -Leaf) -eq "README.md") {
+      $header = "# $parentDirName`n"
+      Write-Host "Header for README.md $header"
   }
+
   if ($isNewFile) {
     Write-Host "Setting header for new file"
     Set-Content -Path $FileName -Value $header
@@ -37,6 +46,6 @@ foreach ($FileName in $FileNames) {
       } else {
         Write-Host "Header already exists in file: $FileName"
       }
-  }
-  dos2unix $FileName
-}
+    }
+    dos2unix $FileName
+    }
