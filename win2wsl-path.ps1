49086394ef29c8e2d521e47362f78e7d30c91713
ziajoriii7 <cybershadowprogress@gmail.com>
2024-02-15 16:44:20 -0500

    UPDATE-SCRIPT ./win2wsl-path.ps1 (1); Add multiple features: 1- Accept arguments as relative paths, 2- Accept system variables as arguments, 3- Accept multiple variables in $args

--- win2wsl-path.ps1
+++ win2wsl-path.ps1
@@ -1,22 +1,59 @@
-param (
-    [string]$path = $(Get-Location).Path
-)
-
 function convertTo-WslPath {
-    param(
-        [string]$winPath
-    )
+	param(
+		[string]$winPath
+	)
 
-        $wslPath = $winpath -replace "\\", "/"
+	$wslPath = $winPath -replace "\\","/"
+	$wslPath = $wslPath -replace "^([A-Za-z]):",{
+		"/mnt/" + $_.Groups[1].Value.ToLower()
+	}
 
-        $wslPath = $wslPath -replace "^([A-Za-z]):", {
-        "/mnt/" + $_.Groups[1].Value.ToLower()
-    }
+	return $wslPath
+}
 
-        return $wslPath
-  }
+$currentPath = $(Get-Location).Path
+$finalPaths = @()
 
-  $wslPath = convertTo-WslPath -winPath $path 
-  Set-Clipboard -Value "`"$wslPath`""
-  
-  Write-Host "`"$wslPath`" copied to clipboard."
+foreach ($relativePath in $args) {
+	if ($relativePath -match "^\$") {
+		$varName = $relativePath.TrimStart("$")
+		if (Get-Variable -Name $varName -ErrorAction SilentlyContinue) {
+			$variableValue = (Get-Variable -Name $varName).Value
+			if ($variableValue -is [System.Management.Automation.PathInfo]) {
+				$relativePath = $variableValue.Path
+			} else {
+				$relativePath = $variableValue
+			}
+		}
+	}
+
+	if ([System.IO.Path]::IsPathRooted($relativePath)) {
+		$pathToResolve = $relativePath
+	} else {
+		$pathToResolve = Join-Path -Path $currentPath -ChildPath $relativePath
+	}
+
+	try {
+		$resolvedPath = Resolve-Path $pathToResolve
+		$wslPath = convertTo-WslPath -winPath $resolvedPath.Path
+		$finalPaths += "`"$wslPath`""
+	} catch {
+		Write-Host "$pathToResolve cannot be resolved."
+	}
+}
+
+if ($finalPaths.Count -eq 0) {
+	$wslPath = convertTo-WslPath -winPath $currentPath
+	Set-Clipboard -Value "`"$wslPath`""
+	Write-Host "`"$wslPath`" copied to clipboard."
+} elseif ($finalPaths.Count -eq 1) {
+	Set-Clipboard -Value $finalPaths
+	Write-Host "$finalPaths copied to clipboard."
+} elseif ($finalPaths) {
+	$quotedPaths = $finalPaths -join "`n"
+	Set-Clipboard -Value $quotedPaths
+	Write-Host ($quotedPaths -join "`n") -NoNewline
+	Write-Host " were copied to clipboard."
+} else {
+	Write-Host "No valid paths were found"
+}
