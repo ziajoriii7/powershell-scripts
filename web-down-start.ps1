ef81ba463d4da6a200be7af072008efa901a2bf9
ziajoriii7 <cybershadowprogress@gmail.com>
2024-03-12 14:41:10 -0500

    FIX-IMPROVE web-down-start.ps1; convert double quotes to single quotes to not evaluate, Start-Process correctly, beautify code

--- web-down-start.ps1
+++ web-down-start.ps1
@@ -1,49 +1,47 @@
 
-function DownloadYouTubeVideo($url) {
-    $ytDlpTitleCommand = "yt-dlp --get-title --get-id -- $($url)"
-    $titleAndId = Invoke-Expression $ytDlpTitleCommand
-    $title = $titleAndId[0]
-    $videoId = $titleAndId[1]
+function DownloadYouTubeVideo ($url) {
+	$ytDlpTitleCommand = "yt-dlp --get-title --get-id -- $($url)"
+	$titleAndId = Invoke-Expression $ytDlpTitleCommand
+	$title = $titleAndId[0]
+	$videoId = $titleAndId[1]
 
-    $normalizedTitle = $title -replace "[\/*?""<>|:]", "_"
-    $normalizedTitleWithID = "$normalizedTitle ($videoId)"
+	$normalizedTitle = $title -replace '[\/*?""<>|:]','_'
+	$normalizedTitleWithID = "$normalizedTitle [$videoId]"
 
-    $ytDlpDownloadCommand = "yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' --output '$PWD\$normalizedTitleWithID.%(ext)s' -- $($url)"
-    Invoke-Expression $ytDlpDownloadCommand
+	$ytDlpDownloadCommand = "yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' --output '${PWD}\$normalizedTitleWithID.%(ext)s' -- $($url)"
+	Invoke-Expression $ytDlpDownloadCommand
 
-    $downloadedFilePath = "$PWD\$normalizedTitleWithID.mp4"
+	$downloadedFilePath = "${PWD}\$normalizedTitleWithID.mp4"
 
-    if (Test-Path $downloadedFilePath) {
-        Write-Host "Opening downloaded file: $downloadedFilePath"
-        Start-Process $downloadedFilePath
-    } else {
-        Write-Host "Download did not complete or file not found."
-    }
+	Write-Host "$normalizedTitleWithID.mp4" -NoNewline -ForegroundColor Green
+	Write-Host " started."
+
+	Start-Process $downloadedFilePath
 }
 
-function DownloadFile($url) {
-    if ($url -like "*youtube.com/watch?*" -or $url -like "*youtube.com/*" -or $url -like "*youtu.be/*") {
-        DownloadYouTubeVideo $url
-    } else {
-        try {
-            $FileName = [System.IO.Path]::GetFileName($url)
-            Invoke-WebRequest -Uri $url -OutFile $FileName
-            
-            if (Test-Path $FileName) {
-                Start-Process $FileName
-            } else {
-                Write-Host "File not found: $FileName"
-            }
-        } catch {
-            Write-Host "Error occurred while downloading the file."
-        }
-    }
+function DownloadFile ($url) {
+	if ($url -like '*youtube.com/watch?*' -or $url -like '*youtube.com/*' -or $url -like '*youtu.be/*') {
+		DownloadYouTubeVideo $url
+	} else {
+		try {
+			$FileName = [System.IO.Path]::GetFileName($url)
+			Invoke-WebRequest -Uri $url -OutFile $FileName
+
+			if (Test-Path $FileName) {
+				Start-Process $FileName
+			} else {
+				Write-Host "File not found: $FileName"
+			}
+		} catch {
+			Write-Host "Error occurred while downloading the file."
+		}
+	}
 }
 
 if ($args.Length -gt 0) {
-    foreach ($url in $args){
-        DownloadFile $url
-    }
+	foreach ($url in $args) {
+		DownloadFile $url
+	}
 } else {
-    Write-Host "No URL(s) were entered. Exiting..."
+	Write-Host "No URL(s) were entered. Exiting..."
 }
