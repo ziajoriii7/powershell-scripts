9953d50caf88103c2aa3644d7f3b922fc94e7247
ziajoriii7 <cybershadowprogress@gmail.com>
2024-04-05 10:13:21 -0500

    NEW-SCRIPT / REPLACE: count.ps1; Count lines of text files. REPLACE CountLines.ps1

---
+++ count.ps1
@@ -0,0 +1,54 @@
+
+param (
+    [string[]]$FilePaths
+)
+
+function Count-Lines {
+    param (
+        [string]$File
+    )
+
+    $lineNumber = 0
+
+    try {
+        Get-Content $File -ErrorAction Stop | ForEach-Object {
+            $lineNumber++ 
+            # Write-Host "${lineNumber}: $_"
+        }
+
+        return [PSCustomObject]@{
+            FileName = (Split-Path $File -Leaf)
+            LineCount = $lineNumber
+        }
+    }
+    catch {
+        Write-Host "'$(Split-Path $File -Leaf)' can't be accessed."
+    }
+}
+
+$fileTypes = @("*.ps1", "*.py", "*.cpp", "*.hpp", "*.c", "*.h", "*.cs", "*.md", "*.ipynb", "*.js", "*.json", ".gitignore", "*.norg", "*.html", "*.css", "*.txt")
+$results = @()
+
+if ($FilePaths.Length -eq 0) {
+    $files = Get-ChildItem -Include $fileTypes -Recurse | Sort-Object LastWriteTime -Descending
+    foreach ($file in $files) {
+        $result = Count-Lines -File $file.FullName
+        if ($result -ne $null) {
+            $results += $result
+        }
+    }
+} else {
+    foreach ($path in $FilePaths) {
+        if (Test-Path $path) {
+            $absolutePath = Resolve-Path $path
+            $result = Count-Lines -File $absolutePath
+            if ($result -ne $null) {
+                $results += $result
+            }
+        } else {
+            Write-Host "'$path' doesn't exist."
+        }
+    }
+}
+
+$results | Format-Table -Property FileName, LineCount -AutoSize
