9776be67c27e26742642fd76a66da7ab323bc37e
ziajoriii7 <cybershadowprogress@gmail.com>
2024-04-05 11:21:24 -0500

    UPDATE MSCV.ps1; commented WSL path

--- MSCV.ps1
+++ MSCV.ps1
@@ -1,6 +1,7 @@
-
+#!/usr/bin/pwsh
 
 $currentDir = Get-Location
 & 'C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Launch-VsDevShell.ps1'
 
+# & "/mnt/c/Program Files/Microsoft Visual Studio/2022/Community/Common7/Tools/Launch-VsDevShell.ps1"
 cd $currentDir
